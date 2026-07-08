31950f164ed096c6c4ed4ea9882a64e8a7518fba
yulchanshin <yulchanshin@gmail.com>
2026-07-08 16:52:49 -0700

    feat(graph): add arena graph representation with Node and push (TICKET-100)W

--- engine/src/graph/arena.rs
+++ engine/src/graph/arena.rs
@@ -1,2 +1,20 @@
 //arena.rs
 //!description will be written when i write this file
+
+use crate::graph::node::{Node, OpType};
+
+struct Graph {
+    nodes: Vec<Node>,
+}
+
+impl Graph {
+    fn new() -> Self {
+        Graph { nodes: Vec::new() }
+    }
+
+    fn push(&mut self, node: Node) -> usize {
+        let len: usize = self.nodes.len();
+        self.nodes.push(node);
+        len
+    }
+}
--- engine/src/graph/node.rs
+++ engine/src/graph/node.rs
@@ -1,2 +1,14 @@
 //node.rs
 //!description incoming
+
+pub struct Node {
+    pub op: OpType,
+    pub inputs: Vec<usize>,
+    pub value: f64,
+    pub adjoint: f64,
+}
+
+pub enum OpType {
+    Var,
+    Mul,
+}
